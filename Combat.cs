d025a6435c4e18d8e249bb0494b96eddf0a645e8
navi <melkissappig@gmail.com>
2025-09-07 00:57:59 +0200

    Update combat
    Meer werk aan combat

--- Combat.cs
+++ Combat.cs
@@ -9,12 +9,35 @@ public static class Combat
 
         int damage = random.Next(weapon.MinimumDamage, weapon.MaximumDamage + 1);
         return damage;
-
     }
 
-    public static void Main()
+    public static void PlayerDamageTaken(Player player, Monster monster)
     {
-        // hi
+        player.CurrentHitPoints -= monster.MaximumDamage;
+    }
+
+    public static void MonsterDamageTaken(Monster monster, Weapon weapon)
+    {
+        int damage = DamageGenerator(weapon);
+        monster.CurrentHitPoints -= damage;
+    }
+
+    public static void Main(Player player, Monster monster, Weapon weapon)
+    {
+        while (player.CurrentHitPoints != 0 || monster.CurrentHitPoints != 0)
+        {
+            Console.WriteLine(@$"
+    {player.Name} HP: {player.CurrentHitPoints}
+    {monster.Name} HP: {monster.CurrentHitPoints}
+    THIS WILL HURT.
+        ");
+
+            Console.ReadLine();
+
+            
+
+
+        }
     }
 
 }
