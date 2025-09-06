61ac6139b54b559a0cb0b54dac95cd5aac37676c
navi <melkissappig@gmail.com>
2025-09-06 23:55:32 +0200

    Setting up combat
    Voor combat heb ik Player, Monster, en Weapon ff nodig dus die ga ik heel standaard klaarzetten. Combat wordt gemaakt, randomizer en een damage output.

--- Monster.cs
+++ Monster.cs
@@ -5,3 +5,22 @@ int MaximumDamage
 int CurrentHitPoints
 int MaximumHitPoints
 */
+
+public class Monster
+{
+    public int ID;
+    public string Name;
+    public int MaximumDamage;
+    public int CurrentHitPoints;
+    public int MaximumHitPoints;
+
+    public Monster(int id, string name, int maxDamage, int currentHp, int maxHp)
+    {
+        ID = id;
+        Name = name;
+        MaximumDamage = maxDamage;
+        CurrentHitPoints = currentHp;
+        MaximumHitPoints = maxHp;
+    }
+}
+
