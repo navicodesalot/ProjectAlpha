fb9c18da33f3338ed60affa8cbd5118cd063a1cf
navi <melkissappig@gmail.com>
2025-09-07 23:56:04 +0200

    Monster damage alteration
    Monsters krijgen nu ook randomized damage tussen twee getallen in :D

--- Monster.cs
+++ Monster.cs
@@ -10,14 +10,16 @@ public class Monster
 {
     public int ID;
     public string Name;
+    public int MinimumDamage;
     public int MaximumDamage;
     public int CurrentHitPoints;
     public int MaximumHitPoints;
 
-    public Monster(int id, string name, int maxDamage, int currentHp, int maxHp)
+    public Monster(int id, string name, int minDamage, int maxDamage, int currentHp, int maxHp)
     {
         ID = id;
         Name = name;
+        MinimumDamage = minDamage;
         MaximumDamage = maxDamage;
         CurrentHitPoints = currentHp;
         MaximumHitPoints = maxHp;
